4f415f08d725d48445bf3243076d853b0f8092a8
Sandro Vecchiarelli <sandrov@live.it>
2017-09-12 17:00:24 +0200

    Cors Enabled

--- WebAPINet4/App_Start/WebApiConfig.cs
+++ WebAPINet4/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace WebAPINet4
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
--- WebAPINet4/Controllers/NWController.cs
+++ WebAPINet4/Controllers/NWController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebAPINet4.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class NWController : ApiController
     {
         private CommonNet4.IDBSource _db;
